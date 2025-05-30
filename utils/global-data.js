fdbb283088eed08231597a888d40bdae95db2218
systemais-admin <max.uljanovs@gmail.com>
2025-05-30 21:16:54 +0100

    new arctical add

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Systemais';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Projects';
+    : 'Projects & Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
