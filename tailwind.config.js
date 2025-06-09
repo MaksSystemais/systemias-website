295efe013f7c3d702d6bce8694d40b721d7cd613
systemais-admin <max.uljanovs@gmail.com>
2025-06-09 19:21:08 +0100

    one article fit on small screen. fix issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,53 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100%',
+            fontSize: '1rem',
+            '@screen sm': {
+              fontSize: '1.125rem',
+            },
+            '@screen md': {
+              fontSize: '1.25rem',
+            },
+            p: {
+              marginTop: '1.25em',
+              marginBottom: '1.25em',
+            },
+            h1: {
+              fontSize: '1.75em',
+              '@screen sm': {
+                fontSize: '2em',
+              },
+              '@screen md': {
+                fontSize: '2.25em',
+              },
+            },
+            h2: {
+              fontSize: '1.5em',
+              '@screen sm': {
+                fontSize: '1.75em',
+              },
+              '@screen md': {
+                fontSize: '2em',
+              },
+            },
+            h3: {
+              fontSize: '1.25em',
+              '@screen sm': {
+                fontSize: '1.5em',
+              },
+              '@screen md': {
+                fontSize: '1.75em',
+              },
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
