101c07de854c7af9d61c1d9dd4a4e9f70b23866b
systemais-admin <max.uljanovs@gmail.com>
2025-08-04 21:14:17 +0100

    share log blog update. SEO update

--- components/SEO.js
+++ components/SEO.js
@@ -5,7 +5,18 @@ export default function SEO({ title, description }) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/images/shareAndLog.png" />
+      
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content="/images/shareAndLog.png" />
     </Head>
   );
 }
